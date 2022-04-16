7779c9c76d05a81d4e294eaa007625800cb0a74c
Yaroslav Bolyukin <iam@lach.pw>
2022-04-17 00:52:34 +0300

    docs: update readme with progress and nix build
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- README.md
+++ README.md
@@ -1,13 +1,103 @@
-# TODO
+# Vive Pro 2 linux driver
+
+## Why
+
+Because HTC doesn't care about non-windows users, even if OG Vive worked just fine for them
+
+## How
+
+Current implementation of driver intercepts some calls between SteamVR and common headset driver `driver_lighthouse`, which is used for OG Vive, and makes it work with newer Vive Pro 2
+
+## Progress
+
+### Working
+
+- HMD image - standard interface (DP) used, however there are some things missing in kernel, see patches below
+- Audio output - standard interface used
+- Front facing camera - works, with minor noise/distortion, standard interface (UVC) used
+- Headset, controllers, Vive tracker (tracking) - part of original driver
+- Headset/controllers firmware updates - part of original driver
+
+### TODO
+
+- Configuration utilities - most of reconfiguration (resolution, noise cancelation, brightness) abilities are already reverse-engineered, they just aren't easly configurable, some GUI utility should be written, or at least everything should be added to `steamvr.vrsettings`
+- Microphone noise canceling configuration (implemented, but not exposed to user)
+- Brightness configuration (implemented, not exposed)
+- Focus knob overlay (Some third-party may work though). Focusing does work, but there is no visual helper.
+- Audio output is not targeted to correct device yet (You need to manually switch it every time), it should be possible to implement this feature in this driver however
+- Front facing camera noise - can be solved with some kernel tinkering (UVC driver)
+- Front facing camera distortion - distortion matrix should be submitted to SteamVR
+- Standalone driver/integration with OpenHMD/Monado - most complex parts (tracking) is already implemented in open source, only thing needed - is to port vive pro 2 specific features/tools
+- Vive Wireless Adapter - support may be implemented, i have some ideas about how it should work, however i dont have one for testing, see donate section below, first received donations will be spent on one
+
+### Will not work
+
+- Vive Console software (GUI utilities etc) - this driver own utilities should be used instead
+
 
 ## Installation
 
-- Install dependencies
-- Compile `lensServer`
-- Run `make enable`, and hope it will work
+This driver can be built using nix:
+
+```sh
+nix build .#driver-proxy-release
+```
+
+...or using manual building instructions from here (i dont provide any guarantees about contents of this repo) https://github.com/santeri3700/vive-pro-2-on-linux#install-vive-pro-2-linux-driver-by-certainlach
+
+...or be downloaded from patreon page, see donate section below
+
+And then installed via
+
+```sh
+./install.sh
+```
+
+Due to how lighthouse driver works, you should also install Vive Console from steam, it wouldn't be usable/used however, it just needs to be installed
+https://store.steampowered.com/app/1635730/VIVE_Console_for_SteamVR/
+
+## Configuration
+
+Currently only one setting added to `steamvr.vrsettings`:
+
+`vivepro2.resolution`: `0-5`
+
+- 0 - 2448x1224 90fps
+- 1 - 2448x1224 120fps
+- 2 - 3264x1632 90fps
+- 3 - 3680x1836 90fps
+- 4 - 4896x2448 90fps
+- 5 - 4896x2448 120fps
 
 ## Required kernel patches
 
-- Mark HMD as non-display (Otherwise SteamVR will not be able to occupy display) https://lore.kernel.org/linux-kernel/20220118170037.14584-1-iam@lach.pw/T/#u
+- Mark HMD as non-display (Otherwise SteamVR will not be able to occupy display) https://lore.kernel.org/linux-kernel/20220118170037.14584-1-iam@lach.pw/ - should be fixed in kernel 5.18 with another patch
 
-- Support type VII timings in DisplayID (VIVE reports mode in them) https://lore.kernel.org/linux-kernel/20220118215956.17229-1-iam@lach.pw/T/#e4b19a91cc9229280cf333c88e82dc824947ab1e1
+- Support type VII timings in DisplayID (VIVE reports mode in them) https://lore.kernel.org/linux-kernel/20220118215956.17229-1-iam@lach.pw/ - merged to kernel in 5.18
+
+- Support fixed DSC BPP rate https://lore.kernel.org/linux-kernel/20220220151940.58327-1-iam@lach.pw/, https://lore.kernel.org/linux-kernel/20220220151940.58327-2-iam@lach.pw/ - not yet merged, currently planning to propose it to 5.19, without this patch highest resolution modes will not work
+
+All of those patches are kept up-to-date in this repo, in `kernel-patches` subdirectory
+
+I recommend you to use kernel 5.17+, as there is other issues in older kernels
+
+If you use NixOS, then you can use kernelPatches from this flake:
+```nix
+boot.kernelPatches = vivepro2-linux-driver.kernelPatches;
+```
+
+If you use arch btw, then you can use this kernel package with all required patches applied (i have not tested it, and can't provide any guarantees about contents of this repo): https://github.com/santeri3700/vive-pro-2-on-linux
+
+## Donate
+
+I dont have enough motivation making this thing work for everyone/adding features everyone wants/needs
+
+You can, however, help me to develop this motivation, here: https://patreon.com/0lach
+
+# Thanks
+
+https://github.com/ChristophHaag for initial OpenVR guidance, which helped me to fix some issues
+
+https://github.com/santeri3700 for writing driver/kernel build instructions here: https://github.com/santeri3700/vive-pro-2-on-linux (i don't provide any guarantees about contents of this repo)
+
+Testers, backers, everyone else
--- flake.nix
+++ flake.nix
@@ -3,60 +3,142 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ rust-overlay.overlay ];
+          overlays = [
+            rust-overlay.overlay
+            (final: prev: {
+              rustPlatform = let
+                rust =
+                  (final.buildPackages.rustChannelOf {
+                    date = "2022-04-15";
+                    channel = "nightly";
+                  })
+                  .default
+                  .override {
+                    extensions = ["rust-src"];
+                  };
+              in
+                prev.makeRustPlatform {
+                  rustc = rust;
+                  cargo = rust;
+                };
+            })
+          ];
         };
-        rust = ((pkgs.buildPackages.rustChannelOf { date = "2022-03-14"; channel = "nightly"; }).default.override {
-          targets = [ "x86_64-pc-windows-gnu" ];
-          extensions = [ "rust-src" ];
-        });
         pkgs-mingw = import nixpkgs {
           inherit system;
-          crossSystem = { config = "x86_64-w64-mingw32"; };
+          overlays = [
+            rust-overlay.overlay
+            (final: prev: {
+              # inherit rustPlatform;
+              # https://github.com/NixOS/nixpkgs/issues/149593
+              openssh = prev.openssh.overrideAttrs (prev: {
+                doCheck = false;
+              });
+              rustPlatform = let
+                rust =
+                  (final.buildPackages.rustChannelOf {
+                    date = "2022-04-15";
+                    channel = "nightly";
+                  })
+                  .default
+                  .override {
+                    targets = ["x86_64-pc-windows-gnu"];
+                  };
+              in
+                prev.makeRustPlatform {
+                  rustc = rust;
+                  cargo = rust;
+                };
+            })
+          ];
+          crossSystem = {
+            config = "x86_64-w64-mingw32";
+            arch = "x86_64";
+            libc = "msvcrt";
+            platform = {};
+            openssl.system = "mingw64";
+          };
+          config.allowUnsupportedSystem = true;
         };
-      in
-      rec {
+      in rec {
+        kernelPatches = [
+          {
+            name = "drm-edid-non-desktop";
+            patch = ./kernel-patches/0001-drm-edid-non-desktop.patch;
+          }
+          {
+            name = "drm-edid-type-7-timings";
+            patch = ./kernel-patches/0002-drm-edid-type-7-timings.patch;
+          }
+          {
+            name = "drm-edid-dsc-bpp-parse";
+            patch = ./kernel-patches/0003-drm-edid-dsc-bpp-parse.patch;
+          }
+          {
+            name = "drm-amd-dsc-bpp-apply";
+            patch = ./kernel-patches/0004-drm-amd-dsc-bpp-apply.patch;
+          }
+        ];
+        packages = let
+          version = "0.1.0";
+          src = builtins.path {
+            path = ./.;
+            filter = path: type: baseNameOf path != "flake.nix";
+          };
+          cargoLock = {lockFile = ./Cargo.lock;};
+        in {
+          driver-proxy = with pkgs;
+            rustPlatform.buildRustPackage {
+              inherit version src cargoLock;
+              pname = "vivepro2-driver-proxy";
+              nativeBuildInputs = [pkgconfig];
+              buildInputs = [udev];
+            };
+          lens-server = with pkgs-mingw;
+            rustPlatform.buildRustPackage {
+              inherit version src cargoLock;
+              pname = "vivepro2-lens-server";
+              buildAndTestSubdir = "bin/lens-server";
+            };
+
+          driver-proxy-release = with pkgs;
+            stdenv.mkDerivation {
+              inherit version src;
+              pname = "vivepro2-driver-proxy-release";
+              installPhase = ''
+                cp -r $src/dist/ $out/
+                chmod u+w -R $out
+                cp ${packages.lens-server}/bin/lens-server.exe $out/lens-server/
+                cp ${packages.driver-proxy}/lib/libdriver_lighthouse.so $out
+              '';
+              patchPhase = "true";
+              fixupPhase = "true";
+            };
+        };
         devShells = {
           default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs;[
-              rust
-              cargo-edit
-              hidapi
-              udev
-              libusb
-              pkg-config
-              wine64
-
-              # druid build
-              cmake
-              pkg-config
-              fontconfig
-
-              # slint runtime
-              xorg.libxcb
-              xorg.libX11
-              xorg.libXcursor
-              xorg.libXrandr
-              xorg.libXi
-
-              cairo
-              vulkan-loader
-              vulkan-loader.dev
-            ];
-            LD_LIBRARY_PATH="${pkgs.vulkan-loader}/lib";
-          };
-          mingw = pkgs-mingw.mkShell {
-            nativeBuildInputs = [ rust ];
-            depsBuildBuild = with pkgs; [ wine64 ];
-            buildInputs = with pkgs-mingw; [ windows.pthreads ];
+            nativeBuildInputs = with pkgs; [];
           };
         };
+        devShell = devShells.default;
       }
     );
 }
