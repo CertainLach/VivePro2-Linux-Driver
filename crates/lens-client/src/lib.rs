18edbd4e769f8a67281c133862cfe31c9f83a6a9
Yaroslav Bolyukin <iam@lach.pw>
2023-11-11 22:40:09 +0100

    feat: try to use proton if installed

--- crates/lens-client/src/lib.rs
+++ crates/lens-client/src/lib.rs
@@ -1,15 +1,15 @@
 use std::{
-	env,
-	ffi::OsStr,
+	env::{self, vars_os},
+	ffi::{OsStr, OsString},
 	io,
 	path::{Path, PathBuf},
 	process::{Command, Stdio},
 	result,
 };
 
-use lens_protocol::Client;
+use lens_protocol::ServerClient;
 use serde_json::Value;
-use tracing::info;
+use tracing::{info, warn};
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
@@ -28,18 +28,73 @@ pub enum Error {
 type Result<T> = result::Result<T, Error>;
 
 // TODO: Pass config on startup
-pub fn start_lens_server(config: Value) -> Result<Client> {
+pub fn start_lens_server(config: Value) -> Result<ServerClient> {
 	let server = find_server().ok_or(Error::CantFindServer)?;
 	info!("using lens server at {server:?}");
 
 	if let Some(wine) = env::var_os("WINE") {
-		let res = start_lens_server_with(wine, server, config)?;
+		let res = start_lens_server_with(wine, false, server, config)?;
 		info!("server is working");
 		Ok(res)
 	} else {
+		'proton: {
+			if let Some(steamvr_path) = env::var_os("STEAM_COMPAT_INSTALL_PATH") {
+				let mut path = PathBuf::from(steamvr_path);
+				// Drop /SteamVR
+				path.pop();
+				let mut name = env::var_os("PROTON_VERSION").map(|v| {
+					let mut full = OsString::from("Proton ");
+					full.push(v.as_os_str());
+					full
+				});
+				if name.is_none() {
+					let readdir = std::fs::read_dir(&path)
+						.expect("parent exists, there should be no other reason to fail");
+					let mut candidates = Vec::new();
+					for item in readdir {
+						let Ok(entry) = item else {
+							continue;
+						};
+						// Proton is ascii name
+						let file_name = entry.file_name();
+						let Some(name) = file_name.to_str() else {
+							continue;
+						};
+						if !name.starts_with("Proton ") {
+							continue;
+						}
+						candidates.push((name.to_owned(), file_name))
+					}
+					// Try to find latest version by semverish comparison
+					candidates.sort_unstable_by(|a, b| a.0.cmp(&b.0));
+					if let Some(version) = candidates.into_iter().last() {
+						name = Some(version.1)
+					}
+				}
+				if let Some(name) = name {
+					path.push(name);
+					path.push("proton");
+					info!("trying {} as proton", path.display());
+					let res = start_lens_server_with(path, true, &server, config.clone());
+					match res {
+						Err(Error::Io(io)) if io.kind() == io::ErrorKind::NotFound => {
+							// Only possible if wine not exists
+							break 'proton;
+						}
+						_ => {}
+					}
+					let res = res?;
+					return Ok(res);
+				} else {
+					warn!("failed to find proton")
+				}
+				// .unwrap_or_else(|| {
+				// });
+			}
+		}
 		for wine in ["wine64", "wine"] {
 			info!("trying {wine} as wine");
-			let res = start_lens_server_with(wine, &server, config.clone());
+			let res = start_lens_server_with(wine, false, &server, config.clone());
 			match res {
 				Err(Error::Io(io)) if io.kind() == io::ErrorKind::NotFound => {
 					// Only possible if wine not exists
@@ -56,24 +111,38 @@ pub fn start_lens_server(config: Value) -> Result<Client> {
 
 pub fn start_lens_server_with(
 	wine: impl AsRef<OsStr>,
+	is_proton: bool,
 	server_path: impl AsRef<Path>,
 	config: Value,
-) -> Result<Client> {
+) -> Result<ServerClient> {
 	let server_path = server_path.as_ref();
 	if !server_path.exists() {
 		return Err(Error::ServerExeNotFound(server_path.to_owned()));
 	}
 
-	let child = Command::new(wine)
+	for var in vars_os() {
+		dbg!(var);
+	}
+	let mut child = Command::new(wine);
+	if is_proton {
+		child
+			.arg("run")
+			.arg("start.exe")
+			.arg("/b")
+			.arg("/wait")
+			.arg("/exec");
+	}
+	child
 		// fixme slows down responses
 		.env("WINEDEBUG", "fixme-all")
 		.arg(server_path)
 		.stdin(Stdio::piped())
 		.stdout(Stdio::piped())
-		.stderr(Stdio::inherit())
-		.spawn()?;
+		.stderr(Stdio::inherit());
+	 dbg!(&child);
+	let child = child.spawn()?;
 
-	Ok(Client::open(child, config)?)
+	Ok(ServerClient::open(child, config)?)
 }
 
 pub fn find_server() -> Option<PathBuf> {
